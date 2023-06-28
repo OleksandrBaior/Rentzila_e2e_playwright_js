13b111f04ba3f03a4aa88bc190559803af31dcab
OleksandrBaior <bayor.olexandr@gmail.com>
2023-06-28 21:16:34 +0200

    fix

--- tests/specs/(3) Main Page/C212 - Checking "Послуги" section on the main page.js
+++ tests/specs/(3) Main Page/C212 - Checking "Послуги" section on the main page.js
@@ -25,7 +25,7 @@ test('C212 - Checking "Послуги" section on the main page ', async ({ page
             await expect(mainPage.services.nth(service)).toHaveText(services[service]);
             
             await mainPage.services.nth(service).click();
-            await page.waitForLoadState('domcontentloaded');
+            await page.waitForLoadState();
             await expect(mapPage.secectedFilter).toHaveText(RegExp(`${services[service]}`));
            
             await mapPage.firstUnit.click(); 

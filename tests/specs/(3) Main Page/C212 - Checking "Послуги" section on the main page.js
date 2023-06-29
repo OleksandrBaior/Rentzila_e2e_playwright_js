3129f3d12f947a987ce7e9e947eec70c628b7da7
OleksandrBaior <bayor.olexandr@gmail.com>
2023-06-29 14:27:57 +0200

    fix

--- tests/specs/(3) Main Page/C212 - Checking "Послуги" section on the main page.js
+++ tests/specs/(3) Main Page/C212 - Checking "Послуги" section on the main page.js
@@ -26,7 +26,7 @@ test('C212 - Checking "Послуги" section on the main page ', async ({ page
 
             await mainPage.services.nth(service).click();
             await expect(mapPage.secectedFilter).toHaveText(RegExp(`${services[service]}`));
-            await page.waitForLoadState('domcontentloaded', { timeout: 500 });
+            await page.waitForLoadState();
 
             await mapPage.firstUnit.click();
             await unitPage.checkRelevantServicePresent(services, service)
